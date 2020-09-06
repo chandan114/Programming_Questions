c7bd01cbf36a42786a64f972329f2b08ece83f3d
chandan maurya <47026029+chandan114@users.noreply.github.com>
2020-09-06 17:03:12 +0530

    max_length_of_subarray_all_have_same_element

---
+++ max_length_of_subarray_all_have_same_element.py
@@ -0,0 +1,16 @@
+array = [1, 2, 3, 4, 5, 5, 5, 5, 5, 2, 2, 1, 1]
+maxlen = 1 ;
+c= 1
+
+for i in range(1 , len(array)):
+    if( array[i]==array[i-1]):
+
+        c+=1
+    else:
+
+        c = 1
+    
+
+    maxlen = max (maxlen , c)
+
+print(maxlen)
